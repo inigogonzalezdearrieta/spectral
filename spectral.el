a25c6f40cb4546c4eb2df8856ad8749f187b714d
Inigo Gonzalez de Arrieta <inigo.gonzalezdearrieta@ehu.eus>
2026-05-26 14:50:00 +0200

    Initial definition of conversion factors

---
+++ spectral.el
@@ -0,0 +1,9 @@
+;;; ...  -*- lexical-binding: t -*-
+;;; spectral.el - spectroscopic unit conversion
+
+;; Relations between units
+
+(defvar thz-to-cm-1 33.356
+  "THz to cm^-1 conversion factor.")
+(defvar thz-to-ev 4.1357e-3
+  "THz to eV conversion factor".)
